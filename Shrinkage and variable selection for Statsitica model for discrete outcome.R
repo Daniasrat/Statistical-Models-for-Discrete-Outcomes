7084ad71c0379d0fdb7d798bfb93fba6b9b46bc7
Daniel Asrat <128075262+Daniasrat@users.noreply.github.com>
2026-02-02 16:14:24 +0000

    Add files via upload

---
+++ Shrinkage and variable selection for Statsitica model for discrete outcome.R
@@ -0,0 +1,9 @@
+########install the packages####
+library(tidyverse)
+library(readr)
+
+
+##########loading the dataset ############
+SAheart <- read_csv("C:/Users/danit/Downloads/SAheart.csv")
+View(SAheart)
+
